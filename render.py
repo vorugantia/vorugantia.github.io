abef5aa9649a3c741466adabef1a5ed9d17f404f
Arjun Voruganti <arjun.voruganti@gmail.com>
2023-06-23 12:30:11 +0100

    CODE REFACTORING

---
+++ render.py
@@ -0,0 +1,23 @@
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+# this tells jinja2 to look for templates
+# in the templates subdirectory
+env = Environment(
+    loader = FileSystemLoader('templates'),
+)
+
+input_file = 'index_template.html'
+output_file = 'index.html'
+
+# reading the template
+template = env.get_template(input_file)
+# render the template.
+# in other words, we replace the template tag
+# by the contents of the overfitting file
+rendered = template.render()
+
+# write the result to disk in index.html
+with open(output_file, 'w') as ofile:
+    ofile.write(rendered)
