3742faf7eaf6459ff90311fbbc0c6aa183c1d01d
Arjun Voruganti <arjun.voruganti@gmail.com>
2023-07-12 19:15:43 +0100

    changed port in upload.sh

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eu
-SSH_PORT=28587
+SSH_PORT=45792
 
 rsync -av --progress -e "ssh -p ${SSH_PORT} -o 'UserKnownHostsFile ./known_hosts'" index.html daniels@bore.pub:/var/www/daily-performance-report/
 rsync -av --progress -e "ssh -p ${SSH_PORT} -o 'UserKnownHostsFile ./known_hosts'" js daniels@bore.pub:/var/www/daily-performance-report/
